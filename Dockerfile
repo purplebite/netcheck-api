0fed369b45adc30fbdbefcdd581e48dad128839c
abhijeetyadav2121 <abhijeetyadav212104@gmail.com>
2024-05-31 11:54:52 +0530

    introduced CELERY_BROKER for task handling

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 
 FROM python:3.8-slim
 
-RUN apt-get update && apt-get install -y  netcat-openbsd iputils-ping iw curl wget gnupg1 apt-transport-https dirmngr && rm -rf /var/lib/apt/lists/*
+RUN apt-get update && apt-get install -y  netcat-openbsd iputils-ping iw curl wget gnupg1 apt-transport-https dirmngr redis-server && rm -rf /var/lib/apt/lists/*
 
 ENV API_KEY=""
 
@@ -17,9 +17,9 @@ WORKDIR /app
 
 COPY . /app
 
-RUN pip install --no-cache-dir Flask gunicorn speedtest-cli
+RUN pip install --no-cache-dir Flask gunicorn speedtest-cli redis celery
 
 EXPOSE $PORT
 
 
-CMD ["/bin/sh", "-c", "gunicorn -w 4 -b 0.0.0.0:$PORT app:app"]
+CMD ["/bin/sh", "-c", "gunicorn -w 4 -b 0.0.0.0:$PORT  --timeout 120 app:app & redis-server --daemonize yes & celery -A app.celery worker --loglevel=info"]
--- app.py
+++ app.py
@@ -1,28 +1,29 @@
-from flask import Flask, request, jsonify, current_app
+from flask import Flask, request, jsonify
 import subprocess
 import os
 import logging
 import sys
 import json
+from celery import Celery
 
 app = Flask(__name__)
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
+celery.conf.update(app.config)
 
 # Read API_KEY from environment variable
 API_KEY = os.environ.get('API_KEY', '')
 DEVICE = os.environ.get('DEVICE', '')
-# DEBUG = os.environ.get('DEBUG', '')
 DEBUG = os.environ.get('DEBUG', '').lower() == 'true'
 SERVERID = os.environ.get('SERVERID', '').lower() == 'true'
-# DEDUPLICATE = os.environ.get('DEDUPLICATE', '').lower() == 'true'
 
 logging.basicConfig(
-
     level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s',
     stream=sys.stdout
 )
-
-
 
 def filter_access_points(access_points):
     try:
@@ -30,97 +31,72 @@ def filter_access_points(access_points):
         filtered_access_points = []
         seen_ssids = set()
 
-        # Iterate through access points to find the highest signal strength for each SSID .
         for ap in access_points:
             ssid = ap['SSID']
             signal_strength = ap['signal_strength']
-
-            # Update the signal strength for the SSID if it's higher
             if ssid not in ssid_signal_map or signal_strength > ssid_signal_map[ssid]:
                 ssid_signal_map[ssid] = signal_strength
 
-        # Iterate through access points again to filter out entries with empty SSID and duplicates
         for ap in access_points:
             ssid = ap['SSID']
             signal_strength = ap['signal_strength']
-
-            # Check if the SSID is not empty, it has the highest signal strength, and it's not a duplicate
             if ssid != "" and signal_strength == ssid_signal_map[ssid] and ssid not in seen_ssids:
                 filtered_access_points.append(ap)
                 seen_ssids.add(ssid)
 
         return filtered_access_points
     except Exception as e:
-        print(e)
+        logging.error(str(e))
         return "error"
+
+@celery.task
+def run_speedtest(serverid):
+    try:
+        if serverid:
+            result = subprocess.run(["speedtest-cli", "--server", "24161", "--json"], capture_output=True, text=True)
+        else:
+            result = subprocess.run(["speedtest-cli", "--json"], capture_output=True, text=True)
+        if result.returncode == 0:
+            output_data = json.loads(result.stdout)
+            output_data["status"] = "success"
+            output_data["download_mbps"] = "{:.0f}".format(output_data["download"] / 10**6 * 8)
+            output_data["upload_mbps"] = "{:.0f}".format(output_data["upload"] / 10**6 * 8)
+            return output_data
+        else:
+            return {"status": "error"}
+    except Exception as e:
+        logging.error(str(e))
+        return {"status": "error"}
 
 @app.route('/speedtest', methods=['GET'])
 def speed():
-    api_key = request.args.get('api_key',"")
-
-    # Check if the API key is correct @
+    api_key = request.args.get('api_key', "")
     if api_key != API_KEY:
         return jsonify({'error': 'Invalid API key'}), 401
 
-    if SERVERID :
-        result = subprocess.run(["speedtest-cli","--server","24161","--json"], capture_output=True, text=True)
-    else:
-        result = subprocess.run(["speedtest-cli","--json"], capture_output=True, text=True)
-        
-    if DEBUG:
-        print("------")
-        print(result)
-        print("------")
+    task = run_speedtest.apply_async(args=[SERVERID])
+    result = task.get()
 
-    if result.returncode == 0:
-        # Add status key-value pair for success
-        output_data = json.loads(result.stdout)
-        if DEBUG:
-            print("------")
-            print(output_data)
-            print("------")
-        output_data["status"] = "success"
-        output_data["download_mbps"] = "{:.0f}".format(output_data["download"] / 10**6 * 8)
-        output_data["upload_mbps"] = "{:.0f}".format(output_data["upload"] / 10**6 * 8)
-        modified_output = json.dumps(output_data)
-    else:
-        # Add status key-value pair for error
-        modified_output = json.dumps({"status": "error"})
-
-    # return modified_output
-
-    return jsonify(json.loads(modified_output)), 200
-    
-
+    return jsonify(result), 200
 
 @app.route('/ping/<ip>', methods=['GET'])
 def ping_device(ip):
-    api_key = request.args.get('api_key',"")
-
-    # Check if the API key is correct @
+    api_key = request.args.get('api_key', "")
     if api_key != API_KEY:
         return jsonify({'error': 'Invalid API key'}), 401
 
-    # Ping the device
     response = subprocess.run(['ping', '-c', '1', ip], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-    # Check the return code to determine if the device is up
     if response.returncode == 0:
         return jsonify({'status': 'success'}), 200
     else:
         return jsonify({'status': 'error'}), 404
 
-
-
 @app.route('/tcp-check/<ip>/<port>', methods=['GET'])
 def tcp_check(ip, port):
-    api_key = request.args.get('api_key',"")
-
-    # Check if the API key is correct
+    api_key = request.args.get('api_key', "")
     if api_key != API_KEY:
         return jsonify({'error': 'Invalid API key'}), 401
 
-    # Check if the port is reachable
     try:
         response = subprocess.run(['nc', '-zv', ip, port], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         if response.returncode == 0:
@@ -128,68 +104,43 @@ def tcp_check(ip, port):
         else:
             return jsonify({'status': 'error'}), 404
     except Exception as e:
-        if DEBUG:
-            logging.error(str(e))
-            return jsonify({'status': 'error'}), 500
+        logging.error(str(e))
         return jsonify({'status': 'error'}), 500
 
+@celery.task
+def scan_access_points(device):
+    command = f'iw dev {device} scan | egrep "SSID|signal"'
+    scan_result = subprocess.check_output(command, universal_newlines=True, shell=True)
+    access_points = []
+    current_ap = {}
+    for line in scan_result.split('\n'):
+        try:
+            if 'SSID:' in line:
+                current_ap['SSID'] = line.split('SSID: ')[1]
+                if len(current_ap) == 2:
+                    access_points.append(current_ap.copy())
+                    current_ap = {}
+            elif 'signal:' in line:
+                current_ap['signal_strength'] = float(line.split('signal: ')[1].split(' dBm')[0])
+                if len(current_ap) == 2:
+                    access_points.append(current_ap.copy())
+                    current_ap = {}
+        except Exception as e:
+            logging.error(f"Error parsing line: {line}, Error: {str(e)}")
 
-
+    filtered_access_points = filter_access_points(access_points)
+    return filtered_access_points
 
 @app.route('/accesspoints', methods=['GET'])
 def access_points():
     api_key = request.args.get('api_key', '')
-
-    # Check if the API key is correct
     if api_key != API_KEY:
         return jsonify({'error': 'Invalid API key'}), 401
-    
-    scan_result=''
 
-    try:
-        # Run the command to scan for nearby access points
-        # scan_result = subprocess.check_output(['iw', 'dev', DEVICE, 'scan','|','egrep','SSID|signal'], universal_newlines=True)
-        command = 'iw dev ' + DEVICE + ' scan | egrep \'SSID|signal\''
-        scan_result = subprocess.check_output(command, universal_newlines=True, shell=True)
-        
-        # Extract SSID and signal strength from the scan result
-        access_points = []
-        current_ap = {}
-        for line in scan_result.split('\n'):
-            # print(line) optional 
-            try:
-                if 'SSID:' in line:
-                    current_ap['SSID'] = line.split('SSID: ')[1]
-                    if len(current_ap) == 2:
-                        access_points.append(current_ap.copy())
-                elif 'signal:' in line:
-                    current_ap['signal_strength'] = float(line.split('signal: ')[1].split(' dBm')[0])
-                    if len(current_ap) == 2:
-                        access_points.append(current_ap.copy())
-            except:
-                pass
-        # access_points=access_points[1]
-        if DEBUG and access_points > 0:
-            logging.info(str(access_points))
-        filtered_access_points = filter_access_points(access_points)
-        return jsonify({'status': 'success', 'access_points': filtered_access_points}), 200
-    except subprocess.CalledProcessError as e:
-        # If the device is not found, return an empty list of access points
-        if DEBUG:
-            logging.error(str(e))
-            return jsonify({'status': 'error'}), 500
-        if e.returncode == 237 or 'No such device' in e.output:
-            # return jsonify({'status': 'success', 'access_points': []}), 200
-            return jsonify({'status': 'error'}), 500
-        else:
-            return jsonify({'status': 'error'}), 500
-    except Exception as e:
-        if DEBUG:
-            logging.error(str(e))
-            return jsonify({'status': 'error'}), 500
-        return jsonify({'status': 'error'}), 500
+    task = scan_access_points.apply_async(args=[DEVICE])
+    result = task.get()
 
-
+    return jsonify({'status': 'success', 'access_points': result}), 200
 
 if __name__ == '__main__':
     app.config['API_KEY'] = API_KEY
