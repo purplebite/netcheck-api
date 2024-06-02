0bfa962f19b11a7183c80cd0784a29ea1310d30b
abhijeetyadav2121 <abhijeetyadav212104@gmail.com>
2024-06-02 23:21:51 +0530

    added 20sec cache time

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
 
-cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 5})
+cache = Cache(app, config={'CACHE_TYPE': 'SimpleCache', 'CACHE_DEFAULT_TIMEOUT': 20})
 
 # Read API_KEY from environment variable
 API_KEY = os.environ.get('API_KEY', '')
